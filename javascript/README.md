7ac5d832e59613082868523be50d8d05aec21b3c
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-10-01 19:17:32 +0100

    Bit of work on exercism path

---
+++ javascript/README.md
@@ -0,0 +1,5 @@
+# Install and run tests
+
+https://exercism.io/tracks/javascript/installation
+
+https://exercism.io/tracks/javascript/tests
--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,3 +1,3 @@
 export function twoFer(name = 'you') {
-  return `One for ${name}, one for me.`
+  return `One for ${name}, one for me.`;
 }
