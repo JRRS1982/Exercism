49b57157268e13b5c26100a469ac7dd446fae5f3
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2020-01-04 16:21:44 +0000

    Update of misc files

---
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -0,0 +1,11 @@
+class ResistorColorDuo
+  def initialize
+    @colours = ('black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white')
+  end
+  
+  def self.value(input)
+    input.each do |x|
+      if x == @
+    end
+  end
+end
