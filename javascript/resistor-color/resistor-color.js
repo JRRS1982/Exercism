df887b452cc929a4f7d8ff67c9115c1d559916fe
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2020-01-04 16:19:50 +0000

    Javascript: Resistor Color Complete

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -4,16 +4,36 @@
 //
 
 export const colorCode = (input) => {
-  
   switch (input){
     case 'black':
       return 0;
       break;
-    case 'white':
-      return 9;
+    case 'brown':
+      return 1;
+      break;
+    case 'red':
+      return 2;
       break;
     case 'orange':
       return 3;
+      break;
+    case 'yellow':
+      return 4;
+      break;
+    case 'green':
+      return 5;
+      break;
+    case 'blue':
+      return 6;
+      break;
+    case 'violet':
+      return 7;
+      break;
+    case 'grey':
+      return 8;
+      break;
+    case 'white':
+      return 9;
       break;
   }
   
--- javascript/resistor-color/resistor-color.spec.js
+++ javascript/resistor-color/resistor-color.spec.js
@@ -6,12 +6,40 @@ describe('ResistorColor', () => {
       expect(colorCode("black")).toEqual(0)
     })
 
-    test('White', () => {
-      expect(colorCode("white")).toEqual(9)
+    test('Brown', () => {
+      expect(colorCode("brown")).toEqual(1)
+    })
+
+    test('Red', () => {
+      expect(colorCode("red")).toEqual(2)
     })
 
     test('Orange', () => {
       expect(colorCode("orange")).toEqual(3)
+    })
+
+    test('Yellow', () => {
+      expect(colorCode("yellow")).toEqual(4)
+    })
+
+    test('Green', () => {
+      expect(colorCode("green")).toEqual(5)
+    })
+
+    test('Blue', () => {
+      expect(colorCode("blue")).toEqual(6)
+    })
+
+    test('Violet', () => {
+      expect(colorCode("violet")).toEqual(7)
+    })
+
+    test('Grey', () => {
+      expect(colorCode("grey")).toEqual(8)
+    })
+
+    test('White', () => {
+      expect(colorCode("white")).toEqual(9)
     })
   })
 
