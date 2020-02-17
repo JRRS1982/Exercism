89577bafb34d1f5cec21df44095edacc5a13253c
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2020-02-17 20:36:16 +0000

    JS: triangle update

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -6,25 +6,28 @@ export class Triangle {
   }
 
   kind() {
-    if (this.first === 0 || this.second === 0 || this.third === 0) {
-      throw 'a triange must have three sides';
-    } else if (this.first < 0 || this.second < 0 || this.third < 0) {
-      throw 'illegal';
-    } else if ( this.first + this.second <= this.third ||
-        this.second + this.third <= this.first ||
-        this.first + this.third <= this.second) {
-      throw "illegal";
+    this.sides = [this.first, this.second, this.third].sort((a, b) => a - b);
+
+    if (this.first === 0 || this.first < 0) {
+        throw 'illegal';
     }
 
+    if (this.first + this.second <= this.third 
+        || this.second + this.third <= this.first 
+        || this.first + this.third <= this.second) {
+      throw 'illegal';
+    }
+    
+    const TRIAGLE_TYPES = { 1: 'equilateral', 2: 'isosceles', 3: 'scalene' };
+    
     if (this.first === this.second && this.second === this.third ) {
-      return "equilateral";
+        return TRIAGLE_TYPES[1];
     } else if (this.first === this.second 
       || this.first === this.third 
       || this.second === this.third) {
-      return "isosceles";
+        return TRIAGLE_TYPES[2];
     } else {
-      return "scalene";
+        return TRIAGLE_TYPES[3];
     }
   }
 }
-
